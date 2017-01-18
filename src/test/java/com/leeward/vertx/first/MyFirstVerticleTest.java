bb16824d5f03683056103325161e632d07326fd1
U-MODOTDS\dirksm2 <dirksm2@COVM00313.dot.missouri>
2017-01-18 15:59:14 -0600

    initial commit

---
+++ src/test/java/com/leeward/vertx/first/MyFirstVerticleTest.java
@@ -0,0 +1,48 @@
+package com.leeward.vertx.first;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.fasterxml.jackson.databind.cfg.ConfigFeature;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+
+@RunWith(VertxUnitRunner.class)
+public class MyFirstVerticleTest {
+	
+	private Vertx vertx;
+	private int port = 8082;
+	
+	@Before
+	public void setUp(TestContext context) {
+		DeploymentOptions options = new DeploymentOptions().setConfig(new JsonObject().put("http.port", port));
+		vertx = Vertx.vertx();
+		vertx.deployVerticle(MyFirstVerticle.class.getName(), options, context.asyncAssertSuccess());
+	}
+
+	@After
+	public void tearDown(TestContext context) {
+		vertx.close(context.asyncAssertSuccess());
+	}
+	
+	@Test
+	public void testMyApplication(TestContext context) {
+		final Async async = context.async();
+		
+		vertx.createHttpClient().getNow(port, "localhost", "/", 
+			response -> {
+				response.handler(body -> {
+					context.assertTrue(body.toString().contains("Hello"));
+					async.complete();
+				});
+			});
+	}
+	
+}
