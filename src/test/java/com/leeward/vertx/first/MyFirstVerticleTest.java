aba0a1676a6dd5f8f91cc0d9b38e13f19bf35311
dirksm2 <Michael.Dirks@modot.mo.gov>
2017-01-25 16:37:18 -0600

    new stuff.

--- src/test/java/com/leeward/vertx/first/MyFirstVerticleTest.java
+++ src/test/java/com/leeward/vertx/first/MyFirstVerticleTest.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.cfg.ConfigFeature;
 
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
@@ -45,17 +46,40 @@ public class MyFirstVerticleTest {
 			});
 	}
 	
-/**	@Test
+	@Test
 	public void checkThatTheIndexPageIsServed(TestContext context) {
 		final Async async = context.async();
 		vertx.createHttpClient().getNow(port, "localhost","/assets/index.html", response -> {
 			context.assertEquals(response.statusCode(), 200);
 			context.assertEquals(response.headers().get("content-type"), "text/html");
 			response.bodyHandler(body -> {
-				context.assertTrue(body.toString().contains("Whiskey"));
+				context.assertTrue(body.toString().contains("<title>My Whisky Collection</title>"));
 				async.complete();
 			});
 		});
 	}
-	*/
+	
+	@Test
+	public void checkThatWeCanAdd(TestContext context) {
+		final Async async = context.async();
+		final String json = Json.encodePrettily(new Whisky("Jameson", "Ireland"));
+		final String length = Integer.toString(json.length());
+		vertx.createHttpClient().post(port, "localhost","/api/whiskies")
+		.putHeader("content-type", "application/json")
+		.putHeader("content-length", length)
+		.handler(response -> {
+			context.assertEquals(response.statusCode(), 201);
+			context.assertTrue(response.headers().get("content-type").contains("application/json"));
+			response.bodyHandler(body -> {
+				final Whisky whisky = Json.decodeValue(body.toString(), Whisky.class);
+				context.assertEquals(whisky.getName(), "Jameson");
+				context.assertEquals(whisky.getOrigin(), "Ireland");
+				context.assertNotNull(whisky.getId());
+				async.complete();
+			});
+		})
+		.write(json)
+		.end();
+	}
+	
 }
